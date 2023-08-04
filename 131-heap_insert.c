7249d99a0f3d1e69f407773519ec723917f924f2
Browni Wilson <browniwils@gmail.com>
2023-08-04 01:34:46 +0000

    added advanced task 131 for 0x1D

---
+++ 131-heap_insert.c
@@ -0,0 +1,55 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_size - calculates size of a binary tree.
+ * @tree: tree.
+ * Return: size of the tree, otherwise 0
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * heap_insert- inserts a value in Max Binary Heap.
+ * @root: root node.
+ * @value: value to store in the node.
+ * Return: created node, otherwise NULL.
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *tree, *new_n, *flip_n;
+	int size, leafs, sub, bit, level, temp;
+
+	if (!root)
+		return (NULL);
+	if (!(*root))
+		return (*root = binary_tree_node(NULL, value));
+
+	tree = *root;
+	size = binary_tree_size(tree);
+	leafs = size;
+
+	for (level = 0, sub = 1; leafs >= sub; sub *= 2, level++)
+		leafs -= sub;
+	for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
+		tree = leafs & bit ? tree->right : tree->left;
+
+	new_n = binary_tree_node(tree, value);
+	leafs & 1 ? (tree->right = new_n) : (tree->left = new_n);
+
+	flip_n = new_n;
+	for (; flip_n->parent && (flip_n->n > flip_n->parent->n); flip_n = flip_n->parent)
+	{
+		temp = flip_n->n;
+		flip_n->n = flip_n->parent->n;
+		flip_n->parent->n = temp;
+		new_n = new_n->parent;
+	}
+
+	return (new_n);
+}
